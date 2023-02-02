bdad6d0866fe409326d907b5aad4bb07c9a78cd1
NathanPeixoto <nathan.lourencopeixoto2@gmail.com>
2023-02-02 19:59:43 -0300

    primeira página

---
+++ pages/tempo.js
@@ -0,0 +1,12 @@
+function Tempo(){
+    const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toUTCString();
+
+    return(
+        <div>
+            {dynamicDateString}(dinâmico)
+        </div>
+    )
+}
+
+export default Tempo;
